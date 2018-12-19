af792b436d4509d0581daad8a9b83d016eed116e
GengYu Rao <zouyoo@outlook.com>
2018-12-19 09:51:34 +0800

    Update log.hpp

--- logging/log.hpp
+++ logging/log.hpp
@@ -1,4 +1,4 @@
-this file is taken from hotspot source code
+//this file is taken from hotspot source code
 /*
  * Copyright (c) 2015, 2016, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
