d987cf5f1bef98dd07d025027a9d338e41984987
GengYu Rao <zouyoo@outlook.com>
2018-12-19 10:32:52 +0800

    Create timer_example.cpp

---
+++ yoo/timer_example.cpp
@@ -0,0 +1,12 @@
+//https://stackoverflow.com/questions/3220477/how-to-use-clock-in-c
+#include <iostream>
+#include <cstdio>
+#include <ctime>
+
+std::clock_t clock;
+double duration;
+clock = std::clock();
+
+/* core code here*/
+duration = ( std::clock() - clock ) / (double) CLOCKS_PER_SEC;
+std::cout<<"the code took: "<< duration <<'\n';
