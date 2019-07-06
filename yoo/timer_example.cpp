57cc8c177cd31eca463232b84c21a42284f4ea84
Z-Y00 <zouyoo@outlook.com>
2019-07-06 11:20:13 +0800

    Update timer_example.cpp

--- yoo/timer_example.cpp
+++ yoo/timer_example.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstdio>
 #include <ctime>
+#include <iomanip>
 
 std::clock_t clock;
 double duration;
@@ -9,4 +10,4 @@ clock = std::clock();
 
 /* core code here*/
 duration = ( std::clock() - clock ) / (double) CLOCKS_PER_SEC;
-std::cout<<"the code took: "<< duration <<'\n';
+std::cout<<"the code took: "<< duration << std::setprecision(5)<<'\n';
