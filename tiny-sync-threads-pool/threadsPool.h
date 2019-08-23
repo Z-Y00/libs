b684c039bab4c0340c6975465eb8e9c2a1fb289b
Z-Y00 <zouyoo@outlook.com>
2019-08-22 17:44:09 -0700

    Create threadsPool.h

---
+++ tiny-sync-threads-pool/threadsPool.h
@@ -0,0 +1,8 @@
+//the threads will run upon wakeup(), and will sleep when its own works done
+// the works needs to be balanced, thus the cost of sync will be saved
+
+void wakeUp(){}
+
+void finalize(){}
+
+void init(){}//bind the threads to the side of NUMA
